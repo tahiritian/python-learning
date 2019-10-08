81c454fbadf617242f41d303737c8644b6960555
Mohammad Tahir <49408506+tahiritian@users.noreply.github.com>
2019-10-08 15:59:36 -0700

    Create adder.py

---
+++ programs/adder.py
@@ -0,0 +1,13 @@
+ui = input("enter ',' seperated numbers:" ).split(',')
+print("Summation: ", sum(map(int,ui)))
+#print("Summation: ", sum([ int(i) for i in ui]))
+
+
+
+
+#ui = input("enter ',' seperated numbers:" )
+##12,34,56,12
+#ui = ui.split()
+##['12', '34','56','12']
+#m = map(int, ui)
+#print("Summation:", sum(m))
